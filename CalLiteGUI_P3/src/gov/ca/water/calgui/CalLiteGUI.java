bd154da7f7226538842416b79a8b346e8e8fb4d0
Mohan Kumar Velaga <mohan5v5988@gmail.com>
2017-02-07 14:50:18 -0800

    Added javadoc for CalLiteGUI class

--- CalLiteGUI_P3/src/gov/ca/water/calgui/CalLiteGUI.java
+++ CalLiteGUI_P3/src/gov/ca/water/calgui/CalLiteGUI.java
@@ -2,6 +2,12 @@ package gov.ca.water.calgui;
 
 import gov.ca.water.calgui.presentation.CalLiteInitClass;
 
+/**
+ * This is the root class for this application. We start the application using
+ * this class.
+ * 
+ * @author Mohan
+ */
 public class CalLiteGUI {
 	public static void main(String[] args) {
 		CalLiteInitClass calLiteInit = new CalLiteInitClass();
