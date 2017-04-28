1a29d430699c04c11e11b97f64b13e7d2ca4473f
tslawecki <tad@limno.com>
2017-04-28 15:22:34 -0400

    64-bit updates for model_w2

--- CalLiteGUI_P3/CalLite_w2.bat
+++ CalLiteGUI_P3/CalLite_w2.bat
@@ -1,8 +1,10 @@
-@title=CalLiteRunwsidi
-del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\wsidi\run\progress.txt
-del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\wsidi\run\"=WreslCheck_main=.log"
-del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\wsidi\run\"=WreslCheck_main_wsidi=.log"
-del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\wsidi\run\wsidi_iteration.log
+del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\run\wsidi_iteration.log
+del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\run\progress.txt
+del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\run\"=WreslCheck_main=.log"
+del /F /Q C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\run\"=WreslCheck_main_wsidi=.log"
 
+timeout /T 3
+start /wait /min group_0.bat
 
-Model_w2\vscript.bat Model_w2\vscript\Main.py "C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\wsidi\wsidi_wsidi.config" 3
+exit
+
--- CalLiteGUI_P3/group_0.bat
+++ CalLiteGUI_P3/group_0.bat
@@ -1,3 +1,3 @@
-@title = "%~dp0\Model_w2\runConfig_calgui C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\s2\s2.config s2"
-%~dp0\Model_w2\runConfig_calgui C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\s2\s2.config s2
+@title = "%~dp0\Model_w2\runConfig_calgui C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\test.config test"
+%~dp0\Model_w2\runConfig_calgui C:\Users\tslawecki.LIMNO\git\CalLiteGUI_P3-32\CalLiteGUI_P3\Scenarios\Run_Details\test\test.config test
 
